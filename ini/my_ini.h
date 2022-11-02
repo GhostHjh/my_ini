af3e86fdfcbf43f53c64a1f5b6b4986c2883f27f
Ghost <GhostHjh@outlook.com>
2022-11-02 17:59:27 +0800

    基本上完全可用

---
+++ ini/my_ini.h
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using std::string;
+using std::vector;
+using std::map;
+
+class my_ini_value
+{
+private:
+    enum type_t
+    {
+        Nullptr = 0,
+        Struct,
+        Int,
+        Double,
+        Str,
+        Bool
+    };
+    
+public:
+    my_ini_value();
+    my_ini_value(const my_ini_value& argv_tmp_ini_value);
+    my_ini_value(const int& argv_value);
+    my_ini_value(const double& argv_value);
+    my_ini_value(const string& argv_value);
+    my_ini_value(const char* argv_value);
+    my_ini_value(const bool& argv_value);
+
+    void operator = (const int& argv_value);
+    void operator = (const double& argv_value);
+    void operator = (const string& argv_value);
+    void operator = (const char* argv_value);
+    void operator = (const bool& argv_value);
+
+    operator int();
+    operator double();
+    operator const char*();
+    operator string();
+    operator bool();
+
+    string& str();
+
+private:
+    string value;
+    type_t value_type;
+};
+
+class my_ini
+{
+public:
+    my_ini();
+    my_ini(const string& argv_file_name);
+
+    my_ini_value get(const string& argv_ini_block_name, const string& argv_ini_block_key);
+
+public:
+    bool count(const string& argv_key);
+    bool count(const string& argv_name_key, const string& argv_value_name_key);
+    bool push_back(const string& argv_name, const string& argv_key, const string& argv_value);
+    void show();
+    
+
+private:
+    map<string, map<string, my_ini_value>> ini_block;
+};
+
+
+
---
+++ main.cpp
@@ -0,0 +1,93 @@
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <sstream>
+// #include <vector>
+// #include <map>
+// #include <regex>
+
+// using std::cout;
+// using std::endl;
+// using std::stringstream;
+// using std::string;
+// using std::vector;
+// using std::map;
+// using std::regex;
+// using std::smatch;
+
+
+// int main(int argc, char** argv)
+// {
+//     std::ifstream file_in("auth.log");
+//     map<string, int> ip_size;
+
+//     vector<string> vector_log;
+//     string tmp_log_str;
+//     string tmp_log_re_ip;
+
+//     if (!file_in.is_open())
+//         throw std::logic_error("文件打开失败");
+    
+//     regex re_ip("((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}");
+//     smatch re_ip_return_str;
+
+//     for (int i = 1, is_find = 0; std::getline(file_in, tmp_log_str); ++i)
+//     {
+//         is_find = tmp_log_str.find("Failed password");
+
+//         if (is_find != -1)
+//         {
+//             std::regex_search(tmp_log_str, re_ip_return_str, re_ip);
+//             tmp_log_re_ip = re_ip_return_str.str();
+//             vector_log.push_back(tmp_log_re_ip);
+
+//             if (ip_size.count(tmp_log_re_ip) == 1)
+//             {
+//                 ip_size[tmp_log_re_ip] = ip_size[tmp_log_re_ip] +1;
+//             }   
+//             else
+//                 ip_size[tmp_log_re_ip] = 1;
+//         }
+
+//     }
+
+//     cout << vector_log.size() << endl;
+
+//     for (auto a = ip_size.begin(); a != ip_size.end(); ++a)
+//         cout << "爆破ip的为\t" << a->first << "\t爆破的次数为\t" << a->second << endl;
+    
+
+//     file_in.close();
+
+
+//     return 0;
+// }
+
+#include <iostream>
+#include "my_ini.h"
+
+int main(int argc, char** argv)
+{   
+    my_ini tmp("test.ini");
+    tmp.show();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
